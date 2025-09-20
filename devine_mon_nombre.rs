4422d1313a603871b40bce256bb2f9ce18d69d97
AminMezouer <amezouer@gmail.com>
2025-09-20 22:13:20 +0200

    V15

--- devine_mon_nombre.rs
+++ devine_mon_nombre.rs
@@ -1,22 +1,30 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     let nombre = rand::thread_rng().gen_range(1..101);
     println!("Devine mon nombre !");
     println!("Le nombre est {nombre}");
 
-    let mut input = String::new();
+    loop {
+        let mut input = String::new();
 
-    io::stdin()
-        .read_line(&mut input)
-        .unwrap();
+        io::stdin()
+            .read_line(&mut input)
+            .unwrap();
 
-    let conv: u32 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+        let conv: u32 = input.trim().parse().expect("Veuillez entrer un nombre valide");
 
-    match conv.cmp(&nombre) {
-        Ordering::Less => println!("Le nombre est plus grand !"),
-        Ordering::Greater => println!("Le nombre est plus petit !"),
-        Ordering::Equal => println!("Tu as trouvé le bon nombre !"),
+        match conv.cmp(&nombre) {
+            Ordering::Less => println!("Le nombre est plus grand !"),
+            Ordering::Greater => println!("Le nombre est plus petit !"),
+            Ordering::Equal => {
+                println!("Tu as trouvé le bon nombre !");
+                break;
+            }
+        }
     }
+
+    
 }
