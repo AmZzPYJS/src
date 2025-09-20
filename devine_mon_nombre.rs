d5221425b0ce3a232e4f8c2d45170840cf76e50f
AminMezouer <amezouer@gmail.com>
2025-09-20 20:19:10 +0200

    V12

--- devine_mon_nombre.rs
+++ devine_mon_nombre.rs
@@ -1,12 +1,24 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    let mut input = String::new();
+    let nombre = rand::thread_rng().gen_range(1..101);
     println!("Devine mon nombre !");
+    println!("Le nombre est {nombre}");
+
+    let mut input = String::new();
 
     io::stdin()
         .read_line(&mut input)
-        .unwrap(); 
+        .unwrap();
 
-    println!("Tu as saisi : {}", input.trim());
+    let conv: u32 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+    if conv == nombre {
+        println!("Tu as trouvé le bon nombre !");
+    } else if xonv < nombre {
+        println!("Le nombre est plus grand !");
+    } else {
+        println!("Le nombre est plus petit !");
+    }
 }
