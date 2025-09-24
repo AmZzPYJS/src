af97d3e6c1a3e335da5e80af36f950e3c9d9e28b
AminMezouer <amezouer@gmail.com>
2025-09-24 15:13:20 +0200

    V17

--- main.rs
+++ main.rs
@@ -1,3 +1,29 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let nombre = rand::thread_rng().gen_range(1..101);
+    println!("Devine mon nombre !");
+
+    loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .unwrap();
+
+        let conv: u32 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+        match conv.cmp(&nombre) {
+            Ordering::Less => println!("Le nombre est plus grand !"),
+            Ordering::Greater => println!("Le nombre est plus petit !"),
+            Ordering::Equal => {
+                println!("Tu as trouvé le bon nombre !");
+                break;
+            }
+        }
+    }
+
+    
 }
